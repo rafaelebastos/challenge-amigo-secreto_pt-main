3e9cfe5e6e6b17bd141c0db96e151b857372aeba
Rafaele Santos <101651943+rafaelebastos@users.noreply.github.com>
2025-03-09 12:09:03 -0300

    Script feito

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+    let nomeInput = document.getElementById("amigo");
+    let nome = nomeInput.value.trim();
+
+    if (nome == "") {
+        alert("Insira um nome válido");
+        return;
+    }
+
+    nomes.push(nome);
+    atualizarLista();
+    nomeInput.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    nomes.forEach(nome => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (nomes.length == 0) {
+        alert("A lista está vazia! Adicione nomes antes de sortear.");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * nomes.length);
+    let nomeSorteado = nomes[indiceSorteado];
+
+    document.getElementById("resultado").innerHTML = `<li>Amigo Sorteado: ${nomeSorteado}</li>`;
+    document.getElementById("resultado").innerHTML = `<li>Amigo Sorteado: ${nomeSorteado}</li>`;
+}
